93cd2b50fb7b9d83b95f72229397982e2719ec3a
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-05-19 16:43:15 +0200

    aggiunto effetto titolo on click

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 const app = new Vue({
     el: '#app',
     data: {
-        message: "Primi passi Vue"
+        message: "Primi passi Vue",
+        img: "https://miro.medium.com/max/1024/1*vMbqi7D_I9ZufeE-Bh7Fiw.png",
+        classe: "blue",
+    },
+    methods: {
+        changeColor() {
+            if (this.classe === "blue") {
+                this.classe = "red";
+            } else {
+                this.classe = "blue";
+            }
+        }
     }
 })
